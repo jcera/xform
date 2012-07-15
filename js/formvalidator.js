80b9aad9aeab89424b7362e10656da1bd3a22e49
unknown <jose@IT7-Jose.elitesin.com>
2012-07-15 11:07:40 +0800

    committed the current state of the project from my laptop.

---
+++ js/formvalidator.js
@@ -0,0 +1,71 @@
+/*	
+*	TODO 
+*	- create a validation for radio button
+*	- create a validation for checkbox
+*	- create a validation for dropdown
+*	- create a validation for textarea
+*	- create a validation for dates. "not less than problem" (datepicker)
+*/
+
+(function( $ ) {
+	
+		$.fn.validateFields = function( options ) {
+
+			var defaults = {
+				'emptyFieldBgColor' : '#FF6B6B',
+				'error-message' : 'Please complete the form'
+			};
+
+			var settings = $.extend(defaults, options);
+
+			var selectedForm = this;
+
+			initForm();
+
+			return this.submit(function() {
+				var currentElement;
+				var formValid = true;
+				$('.required', this).each(function() {
+					currentElement = this;
+					if ($(currentElement).is('input[type="text"]')) {
+						if ( fieldIsEmpty( $(currentElement).val() ) ) {
+							changeBackgroundColor(currentElement);
+							formValid = false;
+						} else {
+							returnBackgroundColor(currentElement);
+						}
+					}
+				});	
+				return formValid;
+			});
+
+			function returnBackgroundColor(element) {
+				$(element).css('background-color', 'white');
+			}
+
+			function changeBackgroundColor(element) {
+				$(element).css('background-color', defaults.emptyFieldBgColor);
+			}
+
+			function fieldIsEmpty(fieldValue) {
+				fieldValue = $.trim(fieldValue);
+				if (fieldValue == null) {
+					return true;
+				}
+				if (fieldValue == '') {
+					return true;
+				}
+				if (fieldValue.length == 0) {
+					return true;
+				}
+				return false;
+			}
+
+			function initForm() {
+				selectedForm.find('.required').each(function() {
+					$(this).parent().find('label').append('<span style="color: red;">*</span>');
+				});
+			};
+		};
+
+})( jQuery );
